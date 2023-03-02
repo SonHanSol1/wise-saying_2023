273505842ef1a7391494cbc779c9da1c71a14689
SonHanSol1 <gksthf7869@naver.com>
2023-03-02 11:47:10 +0900

    5단계 완료

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,28 +10,42 @@ public class App {
     public App(Scanner sc) {
         this.sc = sc;
     }
-    public void run(){
-        //List<String> command = new ArrayList<>();
+    public void run() {
         System.out.println("== 명언 앱 ==");
 
-        while(true){
-            System.out.printf("명언) ");
+        int lastWiseSayingId = 0;
+        List<WiseSaying> wiseSayings = new ArrayList<>();
+
+        while (true) {
+            System.out.print("명령) ");
+            // trim() : 혹시 있을지 모를 좌우공백제거된 버전으로 주세요.
             String command = sc.nextLine().trim();
 
-            if(command.equals("종료")){
+            if (command.equals("종료")) {
                 break;
-            }
-            else if(command.equals("등록")){
-                System.out.printf("명언 : ");
+            } else if (command.equals("등록")) {
+                int id = lastWiseSayingId + 1;
+                System.out.print("명언 : ");
                 String content = sc.nextLine().trim();
-                System.out.printf("작가 : ");
-                String author = sc.nextLine().trim();
-                System.out.println("1번 명언이 등록되었습니다");
+                System.out.print("작가 : ");
+                String authorName = sc.nextLine().trim();
+
+                WiseSaying wiseSaying = new WiseSaying(id, content, authorName);
+                wiseSayings.add(wiseSaying);
+
+                System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                lastWiseSayingId = id; // 증가
+            } else if (command.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("-".repeat(30));
+
+                for(int i = 0; i <= wiseSayings.size()-1; i++){
+                    WiseSaying wiseSaying = wiseSayings.get(i);
+
+                    System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthorName() ,wiseSaying.getContent());
+                }
+                System.out.printf("생성된 명언 수 : %d\n", wiseSayings.size());
             }
         }
-
-
-
-
     }
 }
