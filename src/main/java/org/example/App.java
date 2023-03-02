17e13a389d3f36c67ce02346abd0deb929fd2f7a
SonHanSol1 <gksthf7869@naver.com>
2023-03-02 10:07:46 +0900

    2단계 구현완료

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    private final Scanner sc;
+
+    public App(Scanner sc) {
+        this.sc = sc;
+    }
+    public void run(){
+        //List<String> command = new ArrayList<>();
+        System.out.println("== 명언 앱 ==");
+
+        while(true){
+            System.out.printf("명언) ");
+            String command = sc.nextLine().trim();
+
+            if(command.equals("종료")){
+                break;
+            }
+            else if(command.equals("등록")){
+                System.out.printf("명언 : ");
+                String content = sc.nextLine().trim();
+                System.out.printf("작가 : ");
+                String author = sc.nextLine().trim();
+            }
+        }
+
+
+
+
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String command;
         Scanner sc = new Scanner(System.in);
-        System.out.println("== 명언 앱 ==");
-        System.out.printf("명언) ");
-        command = sc.nextLine();
-
+        //String command;
+        new App(sc).run();
+        sc.close();
     }
 }
