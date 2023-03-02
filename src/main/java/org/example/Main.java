7f84759ce4a28e21b6942e18e2a9ef6ec687973f
SonHanSol1 <gksthf7869@naver.com>
2023-03-02 09:34:05 +0900

    1단계

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String command;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("== 명언 앱 ==");
+        System.out.printf("명언) ");
+        command = sc.nextLine();
+
+    }
+}
